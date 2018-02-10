43f0154a42ccf74bf87644d0e2d5b7dc2960b030
Andreas Joelsson <andreas.joelsson@gmail.com>
2018-02-10 12:46:12 +0100

    Fix mac

--- restore-large-files.sh
+++ restore-large-files.sh
@@ -3,6 +3,6 @@
 if [ "$HOSTNAME" = gpu02 ]; then
   rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --exclude="*" ../large-items/ .
 else
-  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --exclude="*" andreas@gpu02./large-items/ .
+  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --exclude="*" andreas@gpu02:./large-items/ .
 fi
 

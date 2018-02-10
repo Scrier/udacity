bca49b93477aaac6c3cf0e986521da8336132423
Andreas Joelsson <andreas.joelsson@gmail.com>
2018-02-10 12:42:41 +0100

    Fixes for gpu02

--- backup-large-files.sh
+++ backup-large-files.sh
@@ -1,4 +1,8 @@
 #/bin/sh
 
-rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --exclude="*" . andreas@gpu02:./large-items
+if [ "$HOSTNAME" = gpu02 ]; then
+  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --exclude="*" . ../large-items/
+else
+  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --exclude="*" . andreas@gpu02:./large-items/
+fi
 

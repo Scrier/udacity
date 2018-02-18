71f9b02f060184afd33cd0151d1e7c6d8c10df10
Andreas Joelsson <andreas.joelsson@gmail.com>
2018-02-18 10:54:10 +0100

    Fixed delete and exclude for the rsync files.
    Updated docs some with links to local directory.

--- backup-large-files.sh
+++ backup-large-files.sh
@@ -1,10 +1,12 @@
 #/bin/sh
 
 if [ "$HOSTNAME" = gpu02 ]; then
-  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" \
-    --include="*.bin" --include="*.tar.gz" --include="*.npz" --include="*.zip" --exclude="*" . ../large-items/
+  rsync -zarv --prune-empty-dirs --delete --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" \
+    --include="*.tar.gz" --include="*.bin" --include="*.tar.gz" --include="*.npz" --include="*.zip" --exclude="*" . \
+    ../large-items/
 else
-  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" \
-    --include="*.bin" --include="*.npz" --include="*.zip" --exclude="*" . andreas@gpu02:./large-items/
+  rsync -zarv --prune-empty-dirs --delete --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" \
+    --include="*.tar.gz" --include="*.bin" --include="*.npz" --include="*.zip" --exclude="*" . \
+    andreas@gpu02:./large-items/
 fi
 
--- restore-large-files.sh
+++ restore-large-files.sh
@@ -1,10 +1,17 @@
 #/bin/sh
 
 if [ "$HOSTNAME" = gpu02 ]; then
-  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" \
+  rsync -zarv --delete --delete-after --exclude="Deep.Learning/fast-style-transfer" --exclude="Deep.Learning/DeepLearningFlappyBird" \
+    --exclude="Deep.Learning/deep-learning" --exclude="Deep.Learning/aind2-cnn" \
+    --exclude="Deep.Learning/3.Convulutional-Networks/6.Transfer-Learning-in-Tensorflow/tensorflow_vgg" \
+    --exclude="Deep.Learning/3.Convulutional-Networks/7.Dog-Breed-Classifier/dog-project" \
+    --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" \
     --include="*.bin" --include="*.npz" --include="*.zip" --exclude="*" ../large-items/ .
 else
-  rsync -zarv --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" \
+  rsync -zarv --delete-after --exclude="Deep.Learning/fast-style-transfer" --exclude="Deep.Learning/DeepLearningFlappyBird" \
+    --exclude="Deep.Learning/deep-learning" --exclude="Deep.Learning/aind2-cnn" \
+    --exclude="Deep.Learning/3.Convulutional-Networks/6.Transfer-Learning-in-Tensorflow/tensorflow_vgg" \
+    --exclude="Deep.Learning/3.Convulutional-Networks/7.Dog-Breed-Classifier/dog-project" \
+    --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" \
     --include="*.bin" --include="*.npz" --include="*.zip" --exclude="*" andreas@gpu02:./large-items/ .
 fi
-

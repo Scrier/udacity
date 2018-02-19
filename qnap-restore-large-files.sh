d3a33c13103cdf6ede6ea42480d88a7e49f97839
Andreas Joelsson <andreas.joelsson@gmail.com>
2018-02-19 15:56:45 +0100

    Fix sync to and from qnap by fetching gitignore

--- qnap-restore-large-files.sh
+++ qnap-restore-large-files.sh
@@ -1,26 +1,18 @@
 #/bin/sh
 
-
-# rsync -zarv --delete-after --exclude="Deep.Learning/fast-style-transfer" --exclude="Deep.Learning/DeepLearningFlappyBird"
-# --exclude="Deep.Learning/deep-learning" --exclude="Deep.Learning/aind2-cnn"
-# --exclude="Deep.Learning/3.Convulutional-Networks/6.Transfer-Learning-in-Tensorflow/tensorflow_vgg"
-# --exclude="Deep.Learning/3.Convulutional-Networks/7.Dog-Breed-Classifier/dog-project"
-# --include="*/" --include="*.csv" --include="*.hdf5" --include="*.pickle" --include="*.tar.gz" --include="*.bin"
-# --include="*.npz" --include="*.zip" --exclude="*" andreas@gpu02:./large-items/ .
-
-
-source ./qnap-shared.sh
+source ./.qnap-bash.sh
 
 GetLargeFileExtensions INCLUDE "--include=\"" "\""
 GetGitModulePaths EXCLUDE "--exclude=\"" "\""
 
-ARGS="-zarv -e ssh --delete-after"
+ARGS="-zarv --delete-after"
+FIXED_EXCLUDES="--exclude=\".git/\" --exclude=\".idea/\""
 SERVER="admin@scrier.myqnapcloud.com"
 SOURCE="/share/rsync/udacity/"
 TARGET="."
 
-COMMAND="rsync $ARGS --exclude=\".git/\" $EXCLUDE --include=\"*/\" $INCLUDE --exclude=\"*\" $SERVER:$SOURCE $TARGET"
+COMMAND="rsync $ARGS $FIXED_EXCLUDES $EXCLUDE --include=\"*/\" $INCLUDE --exclude=\"*\" $SERVER:$SOURCE $TARGET"
 
 echo $COMMAND
-echo "rsync -zarv --delete-after --exclude=\"Deep.Learning/fast-style-transfer\" --exclude=\"Deep.Learning/DeepLearningFlappyBird\" --exclude=\"Deep.Learning/deep-learning\" --exclude=\"Deep.Learning/aind2-cnn\" --exclude=\"Deep.Learning/3.Convulutional-Networks/6.Transfer-Learning-in-Tensorflow/tensorflow_vgg\" --exclude=\"Deep.Learning/3.Convulutional-Networks/7.Dog-Breed-Classifier/dog-project\" --include=\"*/\" --include=\"*.csv\" --include=\"*.hdf5\" --include=\"*.pickle\" --include=\"*.tar.gz\" --include=\"*.bin\" --include=\"*.npz\" --include=\"*.zip\" --exclude=\"*\" andreas@gpu02:./large-items/ ."
 
+eval $COMMAND
